2cea9221ff6b7103d2944f66b8469349c0a8dff3
Samuel Onoja <samiodev@icloud.com>
2024-12-27 19:06:59 +0100

    add go to nvim-lsp

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,7 +3,7 @@ return {
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "mini.ai", enabled = false },
   { "petertriho/cmp-git", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = true },
   -- { "bufferline.nvim", enabled = false },
   -- { "noice.nvim", enabled = false },
 }
